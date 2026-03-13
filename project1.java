de7cf18024a109729f311101f57426dae8ab430d
anishasuman804-maker <anisha.suman804@schools.sa.edu.au>
2026-03-13 20:09:50 +1030

    Add files via upload

---
+++ project1.java
@@ -0,0 +1,14 @@
+public class project1 {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        int y = 7;
+
+        int z = x + y;
+
+        System.out.println("The sum is: " + z);
+
+    }
+
+}
